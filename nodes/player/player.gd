4d243a186d229d8c6b682fae38cc525645874147
Lucas Rufino <lucaslrt@outlook.com>
2023-03-18 19:47:09 -0300

    Starting character movement.

---
+++ nodes/player/player.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D
+class_name Player
+
+@onready var player_sprite: Sprite2D = $Sprite
+@export var speed: int = 75
+
+func _physics_process(delta: float):
+	horizontal_movement_env()
+	move_and_slide()
+
+func horizontal_movement_env():
+	var input_direction: float = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+
+	velocity.x = input_direction * speed
+	print(velocity.x)
