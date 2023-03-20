9e0054f9d9bbb871e4c76943e6a6af116eaaf84c
Lucas Rufino <lucaslrt@outlook.com>
2023-03-20 11:14:36 -0300

    Creating class to handle player's animations.

---
+++ nodes/player/player_sprite.gd
@@ -0,0 +1,21 @@
+extends Sprite2D
+class_name PlayerSprite
+
+@export var animation_path: NodePath
+@onready var animation: AnimationPlayer = get_node(animation_path)
+
+func animate(direction: Vector2) -> void:
+	verify_position(direction)
+	horizontal_behavior(direction)
+
+func verify_position(direction: Vector2) -> void:
+	if direction.x > 0:
+		flip_h = false
+	elif direction.x < 0:
+		flip_h = true
+
+func horizontal_behavior(direction: Vector2) -> void:
+	if direction.x != 0:
+		animation.play("run")
+	else:
+		animation.play("idle")
